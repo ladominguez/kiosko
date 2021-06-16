b0fa023e8378b0a7b47f98f88793f46301fbf1e7
Luis A. Dominguez <ladominguez12@gmail.com>
2021-06-16 18:59:57 -0500

    Initial commit

---
+++ kiosk.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+# DRLA These lines did ot work out, I edited the Preference file manually
+#sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+#sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+#/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://ds.iris.edu/seismon/index.phtml http://localhost:8081/page1.html http://localhost:8081/index.html &
+/usr/bin/chromium-browser --noerrdialogs --kiosk --disable-inforbars http://ds.iris.edu/seismon/index.phtml http://localhost:8081/page1.html http://localhost:8081/index.html & 
+while true; do
+	sleep 15
+	xdotool key ctrl+Tab; #xdotool keyup ctrl+Tab;
+	currenttime=$(date +%M)
+	if [[ "$currenttime" == "01" ]]; then
+		
+		xdotool keydown ctrl+r; xdtool keyup ctrl+r;
+		sleep 20
+		xdotool key ctrl+Tab;
+		xdotool keydown ctrl+r; xdtool keyup ctrl+r;
+		sleep 20
+		xdotool key ctrl+Tab;
+		xdotool keydown ctrl+r; xdtool keyup ctrl+r;
+		sleep 25
+	fi
+done
+
